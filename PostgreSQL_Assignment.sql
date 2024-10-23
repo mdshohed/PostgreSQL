b7ee5987c4641e3d8a97525c37fde8a1a3b3e62d
mdshohed <mdshohed170@gmail.com>
2024-10-23 13:18:04 +0600

    5th sql ans added

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -77,3 +77,10 @@ WHERE course_id NOT IN (
   SELECT DISTINCT course_id
   FROM enrollment
 );
+
+-- Query 5: Retrieve the names of students using a limit of 2, starting from the 3rd student.
+SELECT student_name
+FROM students
+ORDER BY student_id
+LIMIT 2
+OFFSET 2;
