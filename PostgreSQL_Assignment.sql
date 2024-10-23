e72a898767b7053bedf80289b0e71e60792589ef
mdshohed <mdshohed170@gmail.com>
2024-10-23 13:18:24 +0600

    6th sql ans added

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -84,3 +84,9 @@ FROM students
 ORDER BY student_id
 LIMIT 2
 OFFSET 2;
+
+-- Query 6: Retrieve the course names and the number of students enrolled in each course.
+SELECT c.course_name, COUNT(e.student_id) AS students_enrolled
+FROM courses c
+LEFT JOIN enrollment e ON c.course_id = e.course_id
+GROUP BY c.course_name;
